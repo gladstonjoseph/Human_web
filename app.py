eef3b67e63c3a1b844e70461e8db24dcdc09a1bc
Gladston Joseph <gladyjoseph@gmail.com>
2025-12-30 14:36:49 -0500

    created environment and requirements.txt

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='192.168.50.52', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True)
